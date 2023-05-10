ceb0454b0fa58b2a15d90a06bdd542f8981f8b31
Soma-dev0808 <ec.fate.gt@gmail.com>
2023-05-10 08:48:06 -0700

    modified scrollbar

--- src/components/pages/task-board/Styles.ts
+++ src/components/pages/task-board/Styles.ts
@@ -2,6 +2,9 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   overflow-x: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
   width: 100%;
   display: flex;
   justify-content: center;
