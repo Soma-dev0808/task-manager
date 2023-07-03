672afe2a8d92c521f9a9b4a541c2c58c1a4f701f
Soma-dev0808 <ec.fate.gt@gmail.com>
2023-07-02 18:15:01 -0700

    Added typeorm options for production

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,17 +1,32 @@
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import path from "path";
 
-// TODO: .env
-export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "soma",
-  password: "test",
-  database: "taskboard",
-  entities: [path.join(__dirname, "../models/*.ts")],
-  synchronize: true,
-});
+const getOptions = () => {
+  const options: DataSourceOptions = {
+    type: "postgres",
+  };
+  if (process.env.DATABASE_URL) {
+    Object.assign(options, {
+      url: process.env.DATABASE_URL,
+      entities: ["dist/models/*.js"],
+      synchronize: false,
+    });
+  } else {
+    Object.assign(options, {
+      host: process.env.DB_HOST || "localhost",
+      port: parseInt(process.env.DB_PORT ?? "5432"),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME || "postgres",
+      entities: [path.join(__dirname, "../models/*.ts")],
+      synchronize: true,
+    });
+  }
+
+  return options;
+};
+
+export const AppDataSource = new DataSource(getOptions());
 
 export const initializeDB = async () => {
   await AppDataSource.initialize();
