39f19c9b75c69ae3dbfd93967af886bfe4e729b4
Soma-dev0808 <ec.fate.gt@gmail.com>
2023-08-15 20:23:10 -0700

    Added api to ad user to a task board

--- server/src/routes/boardRoutes.ts
+++ server/src/routes/boardRoutes.ts
@@ -1,5 +1,9 @@
 import express from "express";
-import { createBoard, getBoards } from "../controllers/TaskBoardController";
+import {
+  createBoard,
+  getBoards,
+  addUserToBoard,
+} from "../controllers/TaskBoardController";
 import { authMiddleware } from "../middleware/authentication";
 
 const router = express.Router();
@@ -65,6 +69,55 @@ const router = express.Router();
  *                   example: Something went wrong!
  */
 router.post("/create", authMiddleware, createBoard);
+
+/**
+ * @swagger
+ * /api/task-board/add-user:
+ *   post:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Add user to a task board
+ *     tags: [Task Boards]
+ *     description: This route allows a authenticated user to add user to a task board.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               user_id:
+ *                 type: string
+ *                 description: User id
+ *               board_id:
+ *                 type: string
+ *                 description: Board id
+ *             required:
+ *               - user_id
+ *               - board_id
+ *     responses:
+ *       201:
+ *         description: User added to board successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User added to board successfully!
+ *       500:
+ *         description: Server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Server error
+ */
+router.post("/add-user", authMiddleware, addUserToBoard);
 
 /**
  * @swagger
