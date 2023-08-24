39762195b07db639340bd606e91490c2439189a3
Soma-dev0808 <ec.fate.gt@gmail.com>
2023-08-23 17:47:26 -0700

    Temporarily rename file

--- server/src/routes/UserRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { authMiddleware } from "../middleware/authentication";
-import { searchUser } from "../controllers/UserController";
-
-const router = express.Router();
-
-router.get("/search-user", authMiddleware, searchUser);
-
-export default router;
